1579292a14feb8c43391d69bbc3efae25a28c373
pawan <pawansonu4@gmail.com>
2017-12-09 18:38:42 +0530

    customize how model is displayed

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish',
+'status')
+admin.site.register(Post, PostAdmin)
+
