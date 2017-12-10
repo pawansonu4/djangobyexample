e46fe6d492223f9dfa406ec69f8df5889eda2afd
pawan <pawansonu4@gmail.com>
2017-12-10 15:28:41 +0530

    email settings 1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pawansonu4@gmail.com'
+EMAIL_HOST_PASSWORD = 'pawansonu41111'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
