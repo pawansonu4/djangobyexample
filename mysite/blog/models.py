01b05a4734e0af16ccd09f1a29186a0db24b7e44
pawan <pawansonu4@gmail.com>
2017-12-23 18:06:39 +0530

    tag feature added

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -29,7 +31,7 @@ class Post(models.Model):
     default='draft')
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
-
+    tags = TaggableManager()
 
 
     class Meta:
